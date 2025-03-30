3a09573ffbcede58cc8158a88e2daeed8dd99c76
Sagar Patel <52515373+sagarbpatel31@users.noreply.github.com>
2025-03-29 17:24:47 -0700

    Update model.py

--- model.py
+++ model.py
@@ -15,9 +15,13 @@ import numpy as np
 from torch import nn
 import random
 
-random.seed(0)
-np.random.seed(0)
-torch.manual_seed(0)
+# random.seed(0)
+# np.random.seed(0)
+# torch.manual_seed(0)
+
+torch.cuda.manual_seed_all(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
